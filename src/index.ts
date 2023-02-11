22ade917f9cc1c7ea5157d68dbe101239f984948
Flávia Penim <flaviapenim@gmail.com>
2023-02-11 20:42:41 +0000

    Add express server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,15 @@
-console.log('Hello');
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port = process.env.PORT;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Typescript + Express Server!!!');
+});
+
+app.listen(port, () => {
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
